56f397b9e7ad875e45389fcf1e9db79cd7d6e77b
Emanuele Biotti <emanuelebiotti@localhost.localdomain>
2019-12-27 15:51:25 +0100

    first commit

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export class UserItem extends Component {
+    render() {
+        const { login, avatar_url, html_url} = this.props.user; 
+        return (
+            <div className="card text-center">
+                <img 
+                    src={avatar_url} 
+                    className="round-img" 
+                    style={{width:'60px'}}
+                />
+                <h3>{login}</h3>
+                <div>
+                    <a href={html_url} className="btn btn-dark btm_sm my-1" >More</a>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default UserItem
