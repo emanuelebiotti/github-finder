71cb05ed154dd8669bc96f5f11a7be401620b46a
Emanuele Biotti <emanuelebiotti@localhost.localdomain>
2020-01-08 14:59:43 +0100

    app.js from class component to functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component, Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Alert from './components/layout/Alert';
@@ -10,14 +10,14 @@ import './App.css';
 import Search from './components/users/Search';
 
 
-class App extends Component {
-  state = {
-    users: [],
-    user: {},
-    repos: [],
-    loading: false,
-    alert: null,
-  }
+const App = () => {
+
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState({});
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [alert, setAlert] = useState(null);
+
 
 /*   async componentDidMount() {
     this.setState({loading: true});
@@ -26,43 +26,45 @@ class App extends Component {
   } */
 
   //search users
-  searchUsers = async text => {
-    this.setState({loading: true})
+  const searchUsers = async text => {
+    setLoading(true);
     const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    this.setState({users: res.data.items, loading: false})
+    setUsers(res.data.items)
+    setLoading(false);
   };
 
   //get single Github user
-  getUser = async (username) => {
-    this.setState({loading: true})
+  const getUser = async (username) => {
+    setLoading(true);
     const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    this.setState({user: res.data, loading: false})
+    setUser(res.data);
+    setLoading(false);
   };
 
   // get users repo
-  getUserRepos = async (username) => {
-    this.setState({loading: true})
+  const getUserRepos = async (username) => {
+    setLoading(true);
     const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    this.setState({repos: res.data, loading: false})
+    setRepos(res.data);
+    setLoading(false);
   };
 
 
   // clear users
-  clearUsers = () => {
-    this.setState({ users: [], loading: false})
+  const clearUsers = () => {
+    setUsers([])
+    setLoading(false);
   }
 
   //Set Alert
-  setAlert = (msg, type) => {
-    this.setState({ alert: {msg, type}})
+  const showAlert = (msg, type) => {
+    setAlert(msg, type)
 
     setTimeout(() => {
-      this.setState({alert: null})
+      setAlert(null)
     }, 1000);
   }
 
-  render() {
-    const { users, loading, alert, user, repos} = this.state;
     return (
       <Router>
       <div className="App">
@@ -73,10 +75,10 @@ class App extends Component {
             <Route exact path='/' render={props => (
               <Fragment>
                 <Search 
-                  searchUsers={this.searchUsers} 
-                  clearUsers={this.clearUsers} 
+                  searchUsers={searchUsers} 
+                  clearUsers={clearUsers} 
                   showClear={users.length > 0 ? true : false}
-                  setAlert={this.setAlert}
+                  setAlert={showAlert}
                 />
                 <Users loading={loading} users={users}/>
               </Fragment>
@@ -85,8 +87,8 @@ class App extends Component {
             <Route exact path='/user/:login' render={props => (
               <User 
                 {...props} 
-                getUser={this.getUser} 
-                getUserRepos={this.getUserRepos}
+                getUser={getUser} 
+                getUserRepos={getUserRepos}
                 repos={repos}
                 user={user} 
                 loading={loading}
@@ -97,7 +99,6 @@ class App extends Component {
       </div>
       </Router>
     );
-  }
 }
 
 export default App;
