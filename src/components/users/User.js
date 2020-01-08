f3041cb4d9f2fa362f0b17ca59f0f382ebb05af4
Emanuele Biotti <emanuelebiotti@localhost.localdomain>
2020-01-08 12:06:23 +0100

    added repos and repoItem components

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,19 +1,15 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import Spinner from '../layout/Spinner';
+import Repos from '../repos/Repos';
 import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
 
-export class User extends Component {
-    componentDidMount() {
-        this.props.getUser(this.props.match.params.login);
-    }
+const User = ({user, loading, getUser, getUserRepos, repos, match}) => {
+    useEffect(()=> {
+        getUser(match.params.login);
+        getUserRepos(match.params.login);
+    }, []);
 
-    static propTypes = {
-        loading: PropTypes.bool,
-        user: PropTypes.object.isRequired,
-        getUser: PropTypes.func.isRequired,
-    }
-    render() {
         const {
             name,
             avatar_url,
@@ -28,9 +24,7 @@ export class User extends Component {
             public_repos,
             public_gists,
             hireable
-        } = this.props.user;
-
-        const {loading} = this.props;
+        } = user;
 
         if (loading) return <Spinner/>
 
@@ -83,9 +77,19 @@ export class User extends Component {
                   <div className="badge badge-light">Public repos: {public_repos}</div>
                   <div className="badge badge-dark">Public gists: {public_gists}</div>                  
                 </div>
+                <Repos repos={repos}/>
             </Fragment>
+            
         )
-    }
+
+}
+
+User.propTypes = {
+    loading: PropTypes.bool,
+    user: PropTypes.object.isRequired,
+    repos: PropTypes.array.isRequired,
+    getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired
 }
 
 export default User
